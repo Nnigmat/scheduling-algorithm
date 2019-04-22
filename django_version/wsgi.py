d82afc7f92466da2c1fd1dddf8a0b49d40fc824a
Nikita <nnigmat@gmail.com>
2019-04-22 19:35:47 +0300

    Change Procfile

--- django_version/wsgi.py
+++ django_version/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_version.settings')
 
 application = get_wsgi_application()
+os.sys.path.append('/home/django_projects/MyProject')
+
+os.sys.path.append('/home/django_projects/MyProject/myproject')
